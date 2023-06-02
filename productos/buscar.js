fd2affa70c03dfb0ea34406887c4d14b3cbe5111
fernandogtorrez <fernandogtorrez@gmail.com>
2023-06-02 12:30:02 -0500

    actualizando cambios

---
+++ productos/buscar.js
@@ -0,0 +1,9 @@
+const {leerJSON,escribirJSON} = require('../data');
+const productos_db = leerJSON()
+
+const buscar = function(nombre){
+    const producto = productos_db.find(producto => producto.nombre.toLowerCase() === nombre.toLowerCase())
+    return producto 
+  }
+  
+module.exports = buscar;
