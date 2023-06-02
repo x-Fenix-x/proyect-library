3c3da43254dba239bc318b2905d75e820932ac15
lucaspausin <lucaspausin@gmail.com>
2023-06-02 15:53:29 -0300

    agregar funcionalidad editar

--- productos/index.js
+++ productos/index.js
@@ -2,10 +2,10 @@ const { leerJSON, escribirJSON } = require("../data");
 const Producto = require("./Producto");
 
 module.exports = {
-  //   productos: leerJSON(),
-  listar: require("./listar"),
-  agregar: require("./agregar"),
-  filtrar: require("./filtrar"),
-  //   editar: require("./editar"),
-  buscar: require("./buscar"),
+	//   productos: leerJSON(),
+	listar: require("./listar"),
+	agregar: require("./agregar"),
+	filtrar: require("./filtrar"),
+	editar: require("./editar"),
+	buscar: require("./buscar"),
 };
